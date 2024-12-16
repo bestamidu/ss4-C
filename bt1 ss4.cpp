1a06cb989e3a031bdcba323b9c885fff98536e76
bestamidu <ynhu22887@gmail.com>
2024-12-16 17:02:54 +0700

    1

---
+++ bt1 ss4.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	int n;
+	printf("nhap n");
+	scanf("%d",&n);
+	if(n>0){
+		printf("%d la so nguyen ",n); 
+		}
+	else{
+		printf("%d la so am232 ",n);
+		}
+		return 0;
+	}
+	
