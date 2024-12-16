71a9ccd6070bfbc0c42baf633d9eb74fde6817d3
bestamidu <ynhu22887@gmail.com>
2024-12-16 17:04:27 +0700

    5

---
+++ bt5 ss4.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int n1, n2, n3;
+    printf("Nhap so thu nhat: ");
+    scanf("%d", &n1);
+    printf("Nhap so thu hai: ");
+    scanf("%d", &n2);
+    printf("Nhap so thu ba: ");
+    scanf("%d", &n3);
+    if ((n3 > n1 && n3 < n2) || (n3 > n2 && n3 < n1)) {
+        printf("So thu ba %d nam trong khoang giua %d va %d.\n", n3, n1, n2);
+    } else {
+        printf("So thu ba %d KHONG nam trong khoang giua %d va %d.\n", n3, n1, n2);
+    }
+    return 0;
+}
