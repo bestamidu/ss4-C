edf797efbb7008feec5a9580fa984f61df2f1c9d
bestamidu <ynhu22887@gmail.com>
2024-12-16 17:03:24 +0700

    2

---
+++ bt2 ss4.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	int n,i;
+	printf("nhap n");
+	scanf("%d",&n);
+	printf("nhap i");
+	scanf("%d",&i);
+    if(n%i==0){
+    	printf("%d la so chan ",n);}
+    else{
+	printf("%d la so le ",n);}
+	}
+	
